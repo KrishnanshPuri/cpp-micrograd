b9d5c067d2ec95d47c1081da1011367912f9dd87
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-08-15 11:21:42 +0530

    backward pass using topological sort

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
    auto w1 = make_shared<Value>(-3.0,"w1");
    auto w2 =make_shared<Value>(-1.0,"w2");
 
-   auto b = make_shared<Value>(6.7,"b");
+   auto b = make_shared<Value>(6.8813735870195432,"b");
 
    auto x1_w1 = x1*w1; x1_w1->label="x1*w1";
    auto x2_w2 = x2*w2; x2_w2->label="x2*w2";
@@ -20,6 +20,24 @@ int main() {
 
     auto out = tanh(n); out->label="out";
 
+    vector<shared_ptr<Value>> topo;
+    unordered_set<shared_ptr<Value>>vis;
+    function<void(shared_ptr<Value>)> toposort;
+
+    toposort =[&](shared_ptr<Value>node){
+        if(!node || vis.count(node)) return;
+        vis.insert(node);
+        for(auto&child:node->prev){
+            toposort(child);
+        }
+        topo.push_back(node);
+    };
+
+    toposort(out);
+    out->grad=1.0;
+    for(auto it=topo.rbegin();it!=topo.rend();++it){
+        (*it)->backward();
+    }
 
     draw_dot(out, "graph.dot");
     system("dot -Tpng graph.dot -o graph.png");
